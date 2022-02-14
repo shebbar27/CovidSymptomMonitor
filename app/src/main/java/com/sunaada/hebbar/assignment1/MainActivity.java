af3b56837d269ca205d16a730d80bfc76e480326
Sunaada Hebbar <shebbarm@asu.edu>
2022-02-14 00:25:19 -0700

    added support for previewing recorded video

--- app/src/main/java/com/sunaada/hebbar/assignment1/MainActivity.java
+++ app/src/main/java/com/sunaada/hebbar/assignment1/MainActivity.java
@@ -21,6 +21,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -28,13 +29,17 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RequiresApi(api = Build.VERSION_CODES.S)
 public class MainActivity extends AppCompatActivity
@@ -42,16 +47,17 @@ public class MainActivity extends AppCompatActivity
 
     private static final int REQUEST_PERMISSIONS_CODE = 27;
     private static final String FINGERTIP_VIDEO_FILENAME = "FingerTipVideo";
-    private static final int VIDEO_RECORDING_DURATION = 45000;
+    private static final int VIDEO_RECORDING_DURATION = 15000;
     private static final NumberFormat DEFAULT_NUMBER_FORMAT = AppUtility.getDefaultNumberFormat();
     private static final String[] PERMISSIONS = {
             Manifest.permission.CAMERA,
             Manifest.permission.RECORD_AUDIO,
     };
 
-    private static String capturedVideoOutputDir;
+    private static File videoCaptureFile;
 
     private PreviewView previewView;
+    private VideoView videoView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private boolean isCameraConfigured = false;
     private boolean heartRateMeasurementInProgress = false;
@@ -59,6 +65,7 @@ public class MainActivity extends AppCompatActivity
     private Camera camera;
     private Float HeartRate;
     private EditText editText;
+    private ExecutorService executorService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,8 +83,9 @@ public class MainActivity extends AppCompatActivity
                 }}
         );
 
-        capturedVideoOutputDir = getApplicationContext().getFilesDir().toString();
+        videoCaptureFile = new File(getApplicationContext().getFilesDir(), FINGERTIP_VIDEO_FILENAME);
         this.editText = findViewById(heart_rate_textview);
+        this.executorService = Executors.newCachedThreadPool();
         this.initializeCamera();
     }
 
@@ -122,8 +130,13 @@ public class MainActivity extends AppCompatActivity
     @SuppressLint("RestrictedApi")
     @Override
     public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
-        Log.i("Video Capture Begins",
-                "Video Capture has been started! Video will be saved in: " + capturedVideoOutputDir + "/" + FINGERTIP_VIDEO_FILENAME);
+        Log.i("Video Saved",
+                "Video has been saved at: " + videoCaptureFile);
+        createAndDisplayToast(this,
+                getString(R.string.video_saved_message) + videoCaptureFile,
+                Toast.LENGTH_LONG);
+        executorService.execute(this::ComputeAndUpdateHeartRate);
+        this.playbackCapturedVideo();
     }
 
     @SuppressLint("RestrictedApi")
@@ -134,6 +147,9 @@ public class MainActivity extends AppCompatActivity
         }
         
         Log.i("Video Capture Failed", message, cause);
+        createAndDisplayToast(this,
+                getString(save_video_error_message) + videoCaptureFile,
+                Toast.LENGTH_LONG);
     }
 
     private void measureHeartRate() {
@@ -142,8 +158,7 @@ public class MainActivity extends AppCompatActivity
             this.updateHeartRateEditText();
             this.startVideoCapture();
             Handler handler = new Handler();
-            handler.postDelayed(this::stopVideoCaptureAndComputeHeartRate, VIDEO_RECORDING_DURATION);
-            this.updateHeartRateEditText();
+            handler.postDelayed(this::stopVideoCapture, VIDEO_RECORDING_DURATION);
         }
     }
 
@@ -190,11 +205,13 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressLint("RestrictedApi")
     private void initializeCamera() {
-        this.previewView = findViewById(rear_camera_view);
+        this.previewView = findViewById(camera_preview_view);
         this.previewView.setImplementationMode(PreviewView.ImplementationMode.PERFORMANCE);
         this.previewView.setScaleType(PreviewView.ScaleType.FIT_CENTER);
         this.cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         this.videoCapture = new VideoCapture.Builder().build();
+        this.videoView = findViewById(playback_video_view);
+        this.videoView.setVisibility(View.INVISIBLE);
         requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS_CODE);
     }
 
@@ -231,35 +248,56 @@ public class MainActivity extends AppCompatActivity
     @SuppressLint("RestrictedApi")
     private VideoCapture.OutputFileOptions getVideoOutputFileOptions() {
         return new VideoCapture.OutputFileOptions
-                .Builder(new File(capturedVideoOutputDir, FINGERTIP_VIDEO_FILENAME))
+                .Builder(videoCaptureFile)
                 .build();
     }
 
     @SuppressLint("RestrictedApi")
     private void startVideoCapture() {
+        Log.d("Capture Started", "Video Capture Started");
+        createAndDisplayToast(this, getString(R.string.video_capture_started_message), Toast.LENGTH_LONG);
         heartRateMeasurementInProgress = true;
         this.camera.getCameraControl().enableTorch(true);
         VideoCapture.OutputFileOptions outputFileOptions = this.getVideoOutputFileOptions();
         this.videoCapture.startRecording(outputFileOptions, getExecutor(this), this);
-        Log.d("Capture Started", "Video Capture Started");
     }
 
     @SuppressLint("RestrictedApi")
     private void stopVideoCapture() {
         this.videoCapture.stopRecording();
         Log.d("Capture Stopped", "Video Capture Stopped");
+        createAndDisplayToast(this, getString(video_capture_completed_message), Toast.LENGTH_LONG);
         this.camera.getCameraControl().enableTorch(false);
-        heartRateMeasurementInProgress = false;
     }
 
-    private void stopVideoCaptureAndComputeHeartRate() {
-        this.stopVideoCapture();
-        this.HeartRate = computeHeartRate(new File(capturedVideoOutputDir,
-                FINGERTIP_VIDEO_FILENAME));
-        this.updateHeartRateEditText();
+    private void ComputeAndUpdateHeartRate() {
+        Log.d("Current Thread",
+                "In" + Thread.currentThread().getStackTrace()[1].getMethodName()
+                        + "Current Thread: " + Thread.currentThread());
+        if(videoCaptureFile.exists()) {
+            this.HeartRate = computeHeartRate(videoCaptureFile);
+            this.updateHeartRateEditText();
+        }
     }
 
     private void updateHeartRateEditText() {
         this.editText.setText(DEFAULT_NUMBER_FORMAT.format(this.HeartRate));
+    }
+
+    private void playbackCapturedVideo() {
+        Log.d("Current Thread",
+                "In" + Thread.currentThread().getStackTrace()[1].getMethodName()
+                        + "Current Thread: " + Thread.currentThread());
+        MediaController mediaController = new MediaController(this);
+        this.videoView.setMediaController(mediaController);
+        this.videoView.setVisibility(View.VISIBLE);
+        this.previewView.setVisibility(View.INVISIBLE);
+        this.videoView.setVideoURI(Uri.parse(videoCaptureFile.toString()));
+        this.videoView.start();
+        this.videoView.setOnCompletionListener(mediaPlayer -> {
+            this.previewView.setVisibility(View.VISIBLE);
+            this.videoView.setVisibility(View.INVISIBLE);
+            this.heartRateMeasurementInProgress = false;
+        });
     }
 }
