73a2f93cca0082f07557af759d8a35c8b79b8d1f
Sunaada Hebbar <89281237+shebbar27@users.noreply.github.com>
2022-04-01 13:07:57 -0700

    Submissions/assignment2 (#2)
    * Added new login screen
    Added code to get gps location display it as a toast on the home screen
    
    * Added support for background location access, updating database with location and logging network connection speed into file
    
    * Added support for protecting database file with password
    
    * Final submission for aassignment2

---
+++ app/src/main/java/com/sunaada/hebbar/assignment1/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.sunaada.hebbar.assignment1;
+
+import static com.sunaada.hebbar.assignment1.AppUtility.createExitAlertDialogWithConsentAndExit;
+import static com.sunaada.hebbar.assignment1.AppUtility.handleException;
+import static com.sunaada.hebbar.assignment1.R.string.alert_dialog_no;
+import static com.sunaada.hebbar.assignment1.R.string.alert_dialog_yes;
+import static com.sunaada.hebbar.assignment1.R.string.exit_dialog_message;
+import static com.sunaada.hebbar.assignment1.R.string.exit_dialog_title;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.usernameEditText = this.findViewById(R.id.username_edit_text);
+        this.passwordEditText = this.findViewById(R.id.password_edit_text);
+        AppUtility.registerButtonOnClickCallBack(this,
+                this,
+                new ArrayList<Integer>() {{
+                    add(R.id.login_button);
+                    add(R.id.exit_button);
+                }}
+        );
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.exit_button) {
+            this.exitApplication();
+        }
+        else if(this.usernameEditText != null &&
+                this.usernameEditText.getText().toString().equals(getString(R.string.username))  &&
+                this.passwordEditText != null &&
+                this.passwordEditText.getText().toString().equals(getString(R.string.password))) {
+            try {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                this.startActivity(intent);
+            }
+            catch (Exception e) {
+                handleException(e, this);
+            }
+        }
+        else {
+            AppUtility.createAndDisplayToast(this,
+                    getString(R.string.incorrect_username_or_password),
+                    Toast.LENGTH_LONG);
+        }
+    }
+
+    private void exitApplication() {
+        createExitAlertDialogWithConsentAndExit(this,
+                getString(exit_dialog_title),
+                getString(exit_dialog_message),
+                getString(alert_dialog_yes),
+                getString(alert_dialog_no));
+    }
+}
--- app/src/main/java/com/sunaada/hebbar/assignment1/SymptomsDbHelper.java
+++ app/src/main/java/com/sunaada/hebbar/assignment1/SymptomsDbHelper.java
@@ -2,21 +2,20 @@ package com.sunaada.hebbar.assignment1;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import net.sqlcipher.database.SQLiteDatabase;
+import java.io.File;
 
-public class SymptomsDbHelper extends SQLiteOpenHelper {
+public class SymptomsDbHelper {
 
-    public static final int DATABASE_VERSION = 1;
-    private static final String SYMPTOM_MONITOR_DATA_BASE_NAME = "symptomMonitorDB";
+    public static final String SYMPTOM_MONITOR_DATA_BASE_NAME = "symptomMonitorDB";
     public static final String SYMPTOMS_TABLE_NAME = "symptomsTable";
     public static final String RECORD_ID= "recordId";
     public static final String RECORD_ID_KEY= "record_id_key";
 
     private static final String SQL_CREATE_TABLE =
-            "CREATE TABLE " + SYMPTOMS_TABLE_NAME + "("
+            "CREATE TABLE IF NOT EXISTS " + SYMPTOMS_TABLE_NAME + "("
                     + " recordId INTEGER PRIMARY KEY autoincrement, "
                     + " heartRate NUMERIC, "
                     + " respiratoryRate NUMERIC, "
@@ -29,39 +28,49 @@ public class SymptomsDbHelper extends SQLiteOpenHelper {
                     + " symptomsLossOfSmellOrTaste NUMERIC, "
                     + " symptomsCough NUMERIC, "
                     + " symptomsShortnessOfBreath NUMERIC, "
-                    + " symptomsFeelingTired NUMERIC ); ";
+                    + " symptomsFeelingTired NUMERIC, "
+                    + " locationX TEXT, "
+                    + " locationY TEXT, "
+                    + " time TEXT); ";
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + SYMPTOMS_TABLE_NAME;
+    public SQLiteDatabase database;
 
     public SymptomsDbHelper(Context context) {
-        super(context, SYMPTOM_MONITOR_DATA_BASE_NAME, null, DATABASE_VERSION);
-    }
+        SQLiteDatabase.loadLibs(context);
+        File databaseFile = context.getDatabasePath(SYMPTOM_MONITOR_DATA_BASE_NAME);
+        this.database = SQLiteDatabase.openOrCreateDatabase(databaseFile, context.getString(R.string.password), null);
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
         try {
-            db.execSQL(SQL_CREATE_TABLE);
-        }catch (SQLiteException e) {
+            this.database.execSQL(SQL_CREATE_TABLE);
+            Log.d("db transaction success", "SQLite database transaction successful!");
+        }
+        catch (SQLiteException e) {
             e.printStackTrace();
             Log.d("db transaction failed", "SQLite database transaction failed: " + e);
         }
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(SQL_DELETE_ENTRIES);
-        this.onCreate(db);
-    }
-
-    public ContentValues getDatabaseRowForInserting(float heartRate, float respiratoryRate) {
+    public static ContentValues getDatabaseRowForInserting(float heartRate, float respiratoryRate, String time) {
         ContentValues contentValues = new ContentValues();
         contentValues.put("heartRate", heartRate);
         contentValues.put("respiratoryRate", respiratoryRate);
+        contentValues.put("symptomsNausea", 0f);
+        contentValues.put("symptomsHeadache", 0f);
+        contentValues.put("symptomsDiarrhea", 0f);
+        contentValues.put("symptomsSoarThroat", 0f);
+        contentValues.put("symptomsFever", 0f);
+        contentValues.put("symptomsMuscleAche", 0f);
+        contentValues.put("symptomsLossOfSmellOrTaste", 0f);
+        contentValues.put("symptomsCough", 0f);
+        contentValues.put("symptomsShortnessOfBreath", 0f);
+        contentValues.put("symptomsFeelingTired", 0f);
+        contentValues.put("locationX", "0");
+        contentValues.put("locationY", "0");
+        contentValues.put("time", time);
         return contentValues;
     }
 
-    public ContentValues getDatabaseRowForInserting(Float[] symptomsRating) {
+    public static ContentValues getDatabaseRowForUpdating(Float[] symptomsRating) {
         ContentValues contentValues = new ContentValues();
         contentValues.put("symptomsNausea", symptomsRating[0]);
         contentValues.put("symptomsHeadache", symptomsRating[1]);
@@ -73,6 +82,36 @@ public class SymptomsDbHelper extends SQLiteOpenHelper {
         contentValues.put("symptomsCough", symptomsRating[7]);
         contentValues.put("symptomsShortnessOfBreath", symptomsRating[8]);
         contentValues.put("symptomsFeelingTired", symptomsRating[9]);
+        return contentValues;
+    }
+
+    public static ContentValues getEmptyDatabaseRowForInserting() {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("heartRate", 0f);
+        contentValues.put("respiratoryRate", 0f);
+        contentValues.put("symptomsNausea", 0f);
+        contentValues.put("symptomsHeadache", 0f);
+        contentValues.put("symptomsDiarrhea", 0f);
+        contentValues.put("symptomsSoarThroat", 0f);
+        contentValues.put("symptomsFever", 0f);
+        contentValues.put("symptomsMuscleAche", 0f);
+        contentValues.put("symptomsLossOfSmellOrTaste", 0f);
+        contentValues.put("symptomsCough", 0f);
+        contentValues.put("symptomsShortnessOfBreath", 0f);
+        contentValues.put("symptomsFeelingTired", 0f);
+        contentValues.put("locationX", "0");
+        contentValues.put("locationY", "0");
+        contentValues.put("time", "2022-01-01T00:00:00Z");
+        return contentValues;
+    }
+
+    public static ContentValues getDatabaseRowForInserting(String locationX,
+                                                    String locationY,
+                                                    String time) {
+        ContentValues contentValues = getEmptyDatabaseRowForInserting();
+        contentValues.put("locationX", locationX);
+        contentValues.put("locationY", locationY);
+        contentValues.put("time", time);
         return contentValues;
     }
 }
